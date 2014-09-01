786000bbcdf3a2c035eb2098db974db3a7049d88
Arnel A. Borja <arnel@arnelborja.com>
2014-09-02 00:41:50 +0800

    app: Add support for OpenShift

--- app.js
+++ app.js
@@ -17,7 +17,10 @@ var rmdir = require('rimraf');
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port',
+        process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3000);
+app.set('ipaddress',
+        process.env.OPENSHIFT_NODEJS_IP || process.env.IP || '127.0.0.1');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(express.favicon());
@@ -218,7 +221,7 @@ app.post('/upload', function(req, res) {
 });
 
 
-http.createServer(app).listen(app.get('port'), function() {
+http.createServer(app).listen(app.get('port'), app.get('ipaddress'), function() {
     console.log('Express server listening on port ' + app.get('port'));
     console.log('Starting server...');
     start(function(error) {
